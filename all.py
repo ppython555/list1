0549be2a3930986da7be479ebc6001ec8624f6a8
ppython555 <97468147+ppython555@users.noreply.github.com>
2022-01-11 08:34:08 +0400

    Update all.py

--- all.py
+++ all.py
@@ -126,6 +126,33 @@ plt.plot(x, func4)
 plt.plot(x, func)
 plt.show()
 
+#smoot
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arange(-5,5,0.1)# x uzunlugundadi
+randvalues=np.random.rand(len(x))#random qiymetler
+function=-(x+randvalues)**2
+
+
+
+template=np.array([1,1])
+template=np.array([1,1,1])
+template=np.array([1,1,1,1])
+template=np.array([1,1,1,1,1])
+function1=np.convolve(template,function,mode='same')/2
+function2=np.convolve(template,function,mode='same')/3
+function3=np.convolve(template,function,mode='same')/4
+function4=np.convolve(template,function,mode='same')/5
+
+
+plt.plot(x,function1)
+plt.plot(x,function2)
+plt.plot(x,function3)
+plt.plot(x,function4)
+
+plt.show()
+
+
 
 #6. determination
 
